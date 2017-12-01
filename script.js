d0aaeef6f6577646022c685a893c335dcdf114ea
shaancodes <vihari2@gmail.com>
2017-12-01 10:28:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown1").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu1").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu1").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown2").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu2").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu2").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown3").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu3").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu3").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown4").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu4").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu4").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown5").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu5").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu5").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown6").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu6").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu6").hide();
+			}
+		);
+	}
+	
+);
